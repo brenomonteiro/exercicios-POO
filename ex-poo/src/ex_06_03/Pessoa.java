4edfc00717d7247a26e1e08232f2026411056b54
brenomonteiro <brennoo.mendesmonteiro@gmail.com>
2018-03-07 08:42:33 -0300

    exercicios 1 e 2 de poo dia 07/03

---
+++ ex-poo/src/ex_06_03/Pessoa.java
@@ -0,0 +1,14 @@
+package ex_06_03;
+
+public class Pessoa {
+    private String nome;
+    private char sexo;
+    private int idade;
+
+    public Pessoa(String nome, char sexo, int idade) {
+        super();
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+    }
+}
